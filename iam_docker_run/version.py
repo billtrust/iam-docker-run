c420830775d9e40fc535b84b2ff4fbe4f6c4de9c
dwkerwin <dwkerwin@gmail.com>
2019-07-11 11:17:06 -0400

    incremented version number for pypi release

--- iam_docker_run/version.py
+++ iam_docker_run/version.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __title__ = 'iam-docker-run'
 __description__ = 'Run Docker containers within the context of an AWS IAM Role, and other development workflow helpers.'
 __url__ = 'https://github.com/billtrust/iam-docker-run'
